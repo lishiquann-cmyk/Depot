c35f39df9de2fa26cdefb117702489baa172bd92
lishiquann-cmyk <lishiquann@gmail.com>
2026-04-03 10:16:36 +0800

    Initial Commit

---
+++ dev_status_board/dev_status_boardApp.swift
@@ -0,0 +1,17 @@
+//
+//  dev_status_boardApp.swift
+//  dev_status_board
+//
+//  Created by lisq on 2026/4/3.
+//
+
+import SwiftUI
+
+@main
+struct dev_status_boardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
